a0ea4e8e20cef83998fae89d3e83e69de360cc50
rubencosta13 <rubencosta13@users.noreply.github.com>
2021-05-28 19:14:36 +0100

    1

--- script.js
+++ script.js
@@ -1,6 +1,57 @@
+const botao = document.getElementById('btn')
 const weather = document.querySelectorAll('[enviar]')
 const meteo = document.querySelectorAll('[meteo]')
-
-console.log(weather)
-
-meteo.innerText = weather
+const cidade = document.getElementById('cidade')
+const things = document.getElementById('things')
+const darkmode = document.getElementById('darkmode')
+const map = document.getElementById('map').style.visibility = 'hidden'
+const imperial = document.getElementById('imperial')
+const metric = document.getElementById('metric')
+async function fi(op) {
+    var city = document.getElementById('city').value
+    city = city.toString().replace(/\s+/g,"+")
+    const api_loc_id = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=QyCpWNn8DToUOPS57lfjsnnGXgzI4MTQ&q=${city}&language=en-us&details=false`
+    const response = await fetch(api_loc_id)
+    const data = await response.json()
+    const forecast = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${data[0].Key}?apikey=QyCpWNn8DToUOPS57lfjsnnGXgzI4MTQ&language=en-us&details=false&metric=true`
+    const response1 = await fetch(forecast)
+    const condition = await response1.json()
+    const forecast2 = `http://dataservice.accuweather.com/forecasts/v1/daily/1day/${data[0].Key}?apikey=QyCpWNn8DToUOPS57lfjsnnGXgzI4MTQ&language=en-us&details=false&metric=false`
+    const response4 = await fetch(forecast2)
+    const conditionI = await response4.json()
+    const currentcondition = `http://dataservice.accuweather.com/currentconditions/v1/${data[0].Key}?apikey=QyCpWNn8DToUOPS57lfjsnnGXgzI4MTQ&language=en-us&details=true`
+    const response2 = await fetch(currentcondition)
+    const data1 = await response2.json()
+    if(data.wind === '' && data.temperature === ''){
+        alert('Nao tem dados!')
+        document.getElementById('things').style.visibility = 'hidden'
+        return
+    }
+    if(arguments[0] === 'i'){
+        document.getElementById("location").innerText = 'Weather for: '+data[0].EnglishName
+        document.getElementById("windimp").innerText = ''
+        document.getElementById("maxtempimp").innerText = ''
+        document.getElementById("mintempimp").innerText = ''
+        document.getElementById("mintemp").innerText = `Minimum temperature: ${condition.DailyForecasts[0].Temperature.Minimum.Value}°${condition.DailyForecasts[0].Temperature.Maximum.Unit}` 
+        document.getElementById("maxtemp").innerText = `Maximum temperature: ${condition.DailyForecasts[0].Temperature.Maximum.Value}°${condition.DailyForecasts[0].Temperature.Minimum.Unit}`
+        document.getElementById("wind").innerText = `Wind speed: ${data1[0].Wind.Speed.Metric.Value} ${data1[0].Wind.Speed.Metric.Unit}`
+        document.getElementById("desc").innerText = 'Description: '+data1[0].WeatherText
+    }if(arguments[0] === 'm'){
+        document.getElementById("location").innerText = 'Weather for: '+data[0].EnglishName
+        document.getElementById("mintemp").innerText = ''
+        document.getElementById("maxtemp").innerText = ''
+        document.getElementById("wind").innerText = ''
+        document.getElementById("windimp").innerText = `Wind speed: ${data1[0].Wind.Speed.Imperial.Value} ${data1[0].Wind.Speed.Imperial.Unit}`
+        document.getElementById("maxtempimp").innerText = `Maximum temperature: ${conditionI.DailyForecasts[0].Temperature.Maximum.Value}°${conditionI.DailyForecasts[0].Temperature.Maximum.Unit}`
+        document.getElementById("mintempimp").innerText = `Minimum temperature: ${conditionI.DailyForecasts[0].Temperature.Minimum.Value}°${conditionI.DailyForecasts[0].Temperature.Minimum.Unit}`
+        document.getElementById("desc").innerText = 'Description: '+data1[0].WeatherText
+    }
+}
+imperial.addEventListener('click', imperial =>{
+    console.log('Imperial')
+    fi('i')
+})
+metric.addEventListener('click', metric =>{
+    console.log('Metric')
+    fi('m')
+})
